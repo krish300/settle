34337bd2540bdf7ce3592d0dc54d8413be3f156a
Krishna Teja S <krishna.learn@outlook.com>
2020-03-08 15:45:38 +0530

    fix format

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class ExpenseCategory(models.Model):
-    name = models.CharField(max_length=50,unique=True)
+    name = models.CharField(max_length=50, unique=True)
     active = models.BooleanField(default=True)
 
 
