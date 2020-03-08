e5fc238a7acfd4273129bafc392ef770d6deacdd
Krishna Teja S <krishna.learn@outlook.com>
2020-03-08 15:44:45 +0530

    change as per review suggestions

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 
 class ExpenseCategory(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50,unique=True)
     active = models.BooleanField(default=True)
 
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     category = models.ForeignKey(
-        ExpenseCategory, on_delete=models.SET_NULL, null=True)
+        "ExpenseCategory", on_delete=models.SET_NULL, null=True)
     active = models.BooleanField(default=True)
