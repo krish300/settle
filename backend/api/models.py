3311d1890194a2d3a543a522765504d560a12ba6
Krishna Teja S <krishna.learn@outlook.com>
2020-03-08 14:59:22 +0530

    add new models: Product, ExpenseCategory

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
+class ExpenseCategory(models.Model):
+    name = models.CharField(max_length=50)
+    active = models.BooleanField(default=True)
+
+
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(
+        ExpenseCategory, on_delete=models.SET_NULL, null=True)
+    active = models.BooleanField(default=True)
