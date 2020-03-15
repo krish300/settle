be5de90a44dfec0b21d8cb6bb714552921c1182a
KRISHNA TEJA SURAPANENI <krishna.learn@outlook.com>
2020-03-15 06:04:48 +0530

    Revert "Kt.login screen"

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,42 +1,29 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-import Login from "../views/Login.vue";
-import Reports from "../views/Reports.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Login",
-    component: Login
+    path: '/',
+    name: 'Home',
+    component: Home,
   },
   {
-    path: "/home",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/reports",
-    name: "Reports",
-    component: Reports
-  },
-  {
-    path: "/about",
-    name: "About",
+    path: '/about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
