f3f381d20686da65c8ac3917ed239344f4b3163e
krish300 <krishna.learn@outlook.com>
2020-03-28 22:07:13 +0530

    add signals - create a new settlement on delete or close of settlement - set opening cash - set date

--- backend/api/signals.py
+++ backend/api/signals.py
@@ -1,6 +1,7 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
 from .models import Entry, Settlement
+from datetime import datetime, timedelta
 
 
 @receiver([post_save, post_delete], sender=Entry)
@@ -17,3 +18,37 @@ def update_settlement(sender, instance, **info):
                 settlement.cash_expense -= instance.amount
             settlement.expense -= instance.amount
         settlement.save()
+
+
+@receiver(post_save, sender=Settlement)
+def on_settlement_update(sender, instance, **info):
+    # only for updated and NOT created
+    if info.get('created') == False:
+        # on model close
+        if instance.is_closed:
+            create_new_settlement()
+
+
+@receiver(post_delete, sender=Settlement)
+def on_settlement_delete(sender, instance, **info):
+    create_new_settlement()
+
+
+def create_new_settlement():
+    last_settlement = Settlement.objects.all().last()
+    if last_settlement == None:
+        opening_cash = 0
+        date = datetime.now().strftime('%Y-%m-%d')
+    else:
+        last_settlement_date = last_settlement.date
+        opening_cash = last_settlement.closing_cash
+        date = (last_settlement_date + timedelta(days=1)).strftime('%Y-%m-%d')
+    name = f'Settlement For Date: {date}'
+    settlement = Settlement(
+        name=name,
+        date=date,
+        opening_cash=opening_cash,
+        closing_cash=0,
+        last_modified_by='Admin:AutoCreate'
+    )
+    settlement.save()
