49b5403ca8f131bfdd20191df97395b8fdaec7ba
krish300 <krishna.learn@outlook.com>
2020-03-29 14:39:01 +0530

    chnage nav-drawer properties

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <nav>
-      <v-navigation-drawer v-model="drawer" app clipped="clipped">
+      <v-navigation-drawer v-model="drawer" app clipped temporary absolute>
         <v-list dense>
           <!-- <v-list-item v-for="link in links" :key="link.text" router  :to="link.route"> -->
           <v-list-item
